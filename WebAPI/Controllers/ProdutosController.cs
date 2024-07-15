d89ad2fdc29819e1d1c802ca113f4ecf2ee595f2
thiago <thiagohfo@gmail.com>
2024-07-15 17:55:18 -0300

    POST endpoint

--- WebAPI/Controllers/ProdutosController.cs
+++ WebAPI/Controllers/ProdutosController.cs
@@ -28,5 +28,33 @@ namespace WebAPI.Controllers
         
             return produtos;
         }
+
+        [HttpGet("{id:int}", Name = "ObterProduto")]
+
+        public ActionResult<Produto> Get(int id)
+        {
+            var produto = _context.Produtos.FirstOrDefault(p => p.ProdutoId == id);
+            
+            if (produto is null)
+            {
+                return NotFound("Produto não encontrado");
+            }
+
+            return produto;
+        }
+
+        [HttpPost]
+
+        public ActionResult Post(Produto produto)
+        {
+            if (produto is null)
+                return BadRequest();
+            
+            _context.Produtos.Add(produto);
+            _context.SaveChanges();
+            
+            return new CreatedAtRouteResult("ObterProduto", 
+                new { id = produto.ProdutoId }, produto);
+        }
     }
 }
